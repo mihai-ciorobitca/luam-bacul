d92fd50c97ff4194951403753d4a00e871bc3145
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2024-09-09 13:57:57 +0300

    m

---
+++ api/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.use((req, res, next) => {
+  res.status(404).render('page_404');
+});
+
+app.use((req, res, next) => {
+  res.status(404).render('page_500');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on PORT ${port}`);
+});
