4d1a7252bf5dcdb896dd90f5bbc2efa7c79b1614
Mihai <mihai.ciorobitca@outlook.com>
2024-10-10 08:51:25 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/zohoverify', express.static(path.join(__dirname, 'zohoverify')));
 
 app.get("/*", (req, res) => {
-  res.send("404");
+  res.send("suspended");
 });
 
 app.get('/', (req, res) => {
